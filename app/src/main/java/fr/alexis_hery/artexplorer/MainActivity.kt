80d75d297cf05acaac7093484c59789a53cba207
AlexisSauvageau <alexis.sauvageau@outlook.fr>
2023-09-29 09:23:38 +0200

    premier commit

---
+++ app/src/main/java/fr/alexis_hery/artexplorer/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.alexis_hery.artexplorer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
