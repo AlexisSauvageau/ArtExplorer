ddd5953b903d0ca1aea24986a86fead6d1a524df
AlexisSauvageau <alexis.sauvageau@outlook.fr>
2023-10-05 22:47:40 +0200

    amélioration de la bottom navigation view

--- app/src/main/java/fr/alexis_hery/artexplorer/FavorisActivity.kt
+++ app/src/main/java/fr/alexis_hery/artexplorer/FavorisActivity.kt
@@ -1,11 +1,29 @@
 package fr.alexis_hery.artexplorer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class FavorisActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favoris)
+
+        // la page "Favoris" est sélectionnée
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.menu.getItem(1).isChecked = true
+
+        // changer de page
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.home_page -> {
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
