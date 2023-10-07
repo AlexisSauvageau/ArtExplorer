62ecda37eb44fb3665a1230b36a33f452ea5f29d
NiksirDestiny <116752022+NiksirDestiny@users.noreply.github.com>
2023-10-07 18:46:14 +0200

    Modifications JSONStorage
    Suppression du dernier package

--- app/src/main/java/fr/alexis_hery/artexplorer/MainActivity.kt
+++ app/src/main/java/fr/alexis_hery/artexplorer/MainActivity.kt
@@ -7,27 +7,49 @@ import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import fr.alexis_hery.artexplorer.adapter.OeuvreAdapter
+import fr.alexis_hery.artexplorer.storage.OeuvreManager
+
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import java.nio.charset.Charset
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         // bouton pour ajouter une nouvelle oeuvre d'art
         val addOeuvreBtn = findViewById<Button>(R.id.add_button)
         addOeuvreBtn.setOnClickListener {
+
             val intent = Intent(applicationContext, AddOeuvreActivity::class.java)
             startActivity(intent)
         }
+        val data = OeuvreManager(this)
+
+        val lstOeuvres = data.oeuvreList
+
+
+        /*
 
         val lstOeuvres = arrayListOf<OeuvreModel>()
-
         lstOeuvres.add(OeuvreModel("test.png", "La Joconde", "Tableau fait par Leonard de Vinci", "Peinture", false))
         lstOeuvres.add(OeuvreModel("test.png", "Star Wars", "Grande saga de films !", "Cinéma", true))
         lstOeuvres.add(OeuvreModel("test.png", "Le Sphinx", "Une oeuvre intemporelle", "Sculpture", true))
         lstOeuvres.add(OeuvreModel("test.png", "Star Wars", "Grande saga de films !", "Cinéma", false))
         lstOeuvres.add(OeuvreModel("test.png", "Le Sphinx", "Une oeuvre intemporelle", "Sculpture", true))
         lstOeuvres.add(OeuvreModel("test.png", "La Joconde", "Tableau fait par Leonard de Vinci", "Peinture", false))
+        */
+
+
+
+
+
+
 
         // récupérer le recycler view
         val oeuvreRecyclerView = findViewById<RecyclerView>(R.id.lst_oeuvres)
---
+++ app/src/main/java/fr/alexis_hery/artexplorer/storage/OeuvreManager.kt
@@ -0,0 +1,58 @@
+package fr.alexis_hery.artexplorer.storage
+
+import android.content.Context
+import fr.alexis_hery.artexplorer.OeuvreModel
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import java.nio.charset.Charset
+
+class OeuvreManager(private val context: Context) {
+    val oeuvreList: ArrayList<OeuvreModel> = ArrayList()
+
+    init {
+        loadDataFromAssets()
+    }
+
+     private fun getJSONFromAssets(): String? {
+        var json: String?
+        val charset: Charset = Charsets.UTF_8
+        try {
+            val oeuvreJSONFILE = context.assets.open("Data.json")
+            val size = oeuvreJSONFILE.available()
+            val buffer = ByteArray(size)
+            oeuvreJSONFILE.read(buffer)
+            oeuvreJSONFILE.close()
+            json = String(buffer, charset)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return null
+        }
+        return json
+    }
+
+     fun loadDataFromAssets() {
+        try {
+            val json = JSONObject(getJSONFromAssets()!!)
+            val oeuvreTab = json.getJSONArray("Data")
+
+            for (i in 0 until oeuvreTab.length()) {
+                val oeuvre = oeuvreTab.getJSONObject(i)
+                val image = oeuvre.getString("0")
+                val name = oeuvre.getString("1")
+                val description = oeuvre.getString("2")
+                val type = oeuvre.getString("3")
+                val liked = oeuvre.getBoolean("4")
+
+                // Now add all the variables to the data model class and the data model class to the array list.
+                val oeuvreDetails = OeuvreModel(image, name, description, type, liked)
+
+                // add the details in the list
+                oeuvreList.add(oeuvreDetails)
+            }
+        } catch (e: JSONException) {
+            // Handle exception
+            e.printStackTrace()
+        }
+    }
+}
